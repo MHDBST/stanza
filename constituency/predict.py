a42ae8a8ccc45aa5b70b8865ef730a7466d354f2
mbastan <mbastan@cs.stonybrook.edu>
2022-09-14 15:57:28 -0400

    predict on sentence

--- constituency/predict.py
+++ constituency/predict.py
@@ -1,9 +1,12 @@
-from stanza.pipeline.processor import ProcessorVariant, register_processor_variant
 import stanza
 
 # nlp = stanza.Pipeline('en', processors='tokenize,pos', use_gpu=True, pos_batch_size=3000) # Build the pipeline, specify part-of-speech processor's batch size
 # doc = nlp("Barack Obama was born in Hawaii.") # Run the pipeline on the input text
 # print(doc) # Look at the result
+# pipe=stanza.Pipeline('en')
+# print(pipe.processors['constituency'].vocab['constituency'])
+# print(list(pipe.processors['constituency'].vocab['deprel']._unit2id.keys()))
+# stanza.Pipeline.processors['ner'].get_known_tags()
 mdl_pth_out='/home/mbastan/structuralDecoding/neurologic_decoding/stanza/stanza-train/saved_models/constituency/en_out_constituency.pt'
 mdl_pth_my='/home/mbastan/structuralDecoding/neurologic_decoding/stanza/stanza-train/stanza/saved_models/constituency/en_my_constituency.pt'
 sent='Barack Obama was born in Hawaii.'
@@ -23,4 +26,5 @@ while True:
     nlp = stanza.Pipeline(lang='en', processors='tokenize,pos,constituency',model_path=mdl_pth)
     doc = nlp(sent)
     for sentence in doc.sentences:
+        print('original sentence:',sent)
         print(sentence.constituency)
